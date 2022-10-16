2e11faeb3b89d6cf704f7889e67a9dd49b64c180
Luiz Santos <joluizsantos231293@gmail.com>
2022-10-16 01:45:00 -0300

    refactor: :recycle: adjust github image position

--- src/components/GoToGitHub/styles.ts
+++ src/components/GoToGitHub/styles.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { keyframes } from "styled-components";
 
 const RotateIconGit = keyframes`
- 0% { transform: rotate(40deg) }
- 10% {  transform: rotate(347deg) }
- 20% {   transform: rotate(17deg) }
- 30% {   transform: rotate(-60deg)) }
- 40% {  transform: rotate(40deg)  }
- 60% {   transform: rotate(-70deg) }
- 80% {   transform: rotate(50deg) }
- 100% {  transform: rotate(-60deg) } 
+  0% { transform: rotate(40deg) }
+  10% {  transform: rotate(347deg) }
+  20% {   transform: rotate(17deg) }
+  30% {   transform: rotate(-60deg) }
+  40% {  transform: rotate(40deg)  }
+  60% {   transform: rotate(-70deg) }
+  80% {   transform: rotate(50deg) }
+  100% {  transform: rotate(-60deg) } 
 `;
 
 export const GoToGitHubStyle = styled.button`
@@ -40,8 +40,9 @@ export const ImgGit = styled.img`
   width: 50px;
   transform: rotate(-20deg);
   animation-name: ${RotateIconGit};
-  animation-duration: 2.5s;
+  animation-duration: 2.8s;
   animation-iteration-count: infinite;
+  margin-top: 5px;
 
   :hover {
     opacity: 0.7;
